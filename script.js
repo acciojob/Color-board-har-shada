a132a319b75c32bd1c34be0b72a9a6069966ffe0
har-shada <harshadapohane14@gmail.com>
2025-11-26 18:36:06 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,24 @@
 //your JS code here. If required.
+//your JS code here. If 
+
+
+const board = document.getElementById("board");
+
+for (let i = 0; i < 800; i++) {
+  const box = document.createElement("div");
+  box.classList.add("square");
+
+  box.addEventListener("mouseover", () => {
+    box.style.backgroundColor = randomColor();
+
+    setTimeout(() => {
+      box.style.backgroundColor = "#eee";
+    }, 1000);
+  });
+
+  board.appendChild(box);
+}
+
+function randomColor() {
+  return "#" + Math.floor(Math.random() * 16777215).toString(16);
+}
