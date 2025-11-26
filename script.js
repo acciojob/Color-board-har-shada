214aa0b49881c285a6d64878e583624baffe2e77
har-shada <harshadapohane14@gmail.com>
2025-11-26 18:45:00 +0530

    created a submission

--- script.js
+++ script.js
@@ -2,23 +2,45 @@
 //your JS code here. If 
 
 
+//const board = document.getElementById("board");
+
+// for (let i = 0; i < 800; i++) {
+//   const box = document.createElement("div");
+//   box.classList.add("square");
+
+//   box.addEventListener("mouseover", () => {
+//     box.style.backgroundColor = randomColor();
+
+//     setTimeout(() => {
+//       box.style.backgroundColor = "#eee";
+//     }, 1000);
+//   });
+
+//   board.appendChild(box);
+// }
+
+// function randomColor() {
+//   return "#" + Math.floor(Math.random() * 16777215).toString(16);
+// }
 const board = document.getElementById("board");
 
 for (let i = 0; i < 800; i++) {
-  const box = document.createElement("div");
-  box.classList.add("square");
+    const box = document.createElement("div");
+    box.classList.add("square");
 
-  box.addEventListener("mouseover", () => {
-    box.style.backgroundColor = randomColor();
+    box.addEventListener("mouseover", () => {
+        box.style.backgroundColor = randomColor();
 
-    setTimeout(() => {
-      box.style.backgroundColor = "#eee";
-    }, 1000);
-  });
+        setTimeout(() => {
+            box.style.backgroundColor = "#111";
+        }, 1000);
+    });
 
-  board.appendChild(box);
+    board.appendChild(box);
 }
 
 function randomColor() {
-  return "#" + Math.floor(Math.random() * 16777215).toString(16);
+    const colors = ["red", "blue", "yellow", "purple", "green", "cyan", "orange"];
+    return colors[Math.floor(Math.random() * colors.length)];
 }
+
